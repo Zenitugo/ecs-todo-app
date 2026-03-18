e0d83b9858fc066034c050a5d31ddddd9cc2e14f
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-18 19:29:23 +0100

    module updated to include subnet resources

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,7 +1,7 @@
 module "networking" {
   source                              =  "../modules/networking"
   name                                =  var.name
-  vpc_cidr_block                      = var.vpc_cidr_block
-  public_subnet_cidr_block            = var.public_subnet_cidr_block
-  availability_zone                   =     var.availability_zone
+  vpc_cidr_block                      =  var.vpc_cidr_block
+  public_subnet_cidr_block            =  var.public_subnet_cidr_block
+  availability_zone                   =  var.availability_zone
 }
