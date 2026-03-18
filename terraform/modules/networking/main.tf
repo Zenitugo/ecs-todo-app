ccf6a0b3145fa0b2da07aa04e7428f3fccde50bf
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-18 18:49:40 +0100

    vpc resources declared

---
+++ terraform/modules/networking/main.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "ecs_network" {
+  cidr_block       = var.vpc_cidr_block
+  instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.name}-vpc"
+  }
+}
