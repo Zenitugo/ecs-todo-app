f6310e785d3e0198ce6a661a6a7278d63e1aea52
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-19 18:45:29 +0100

    modified the script and exported sg ids of ec2, load balancer and ecs

--- terraform/modules/SG/output.tf
+++ terraform/modules/SG/output.tf
@@ -1,4 +1,16 @@
-output sg_ids{
-  value       = aws_security_group.ecs_todo_app_sg.id
+output load_balancer_sg_ids{
+  value       = aws_security_group.load_balancer_sg.id
+  description = "The name of the security group created for the ECS Todo App."
+}
+
+
+
+output ecs_sg_ids{
+  value       = aws_security_group.ecs_sg.id
+  description = "The name of the security group created for the ECS Todo App."
+}
+
+output ec2_sg_ids{
+  value       = aws_security_group.ec2_sg.id
   description = "The name of the security group created for the ECS Todo App."
 }
