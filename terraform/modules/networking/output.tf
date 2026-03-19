4d1a40582c47a11553287fdae1a4b1b1fa7dbcf3
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-19 14:58:18 +0100

    public subnet id exported

--- terraform/modules/networking/output.tf
+++ terraform/modules/networking/output.tf
@@ -2,3 +2,9 @@ output vpc_id {
   value       = aws_vpc.ecs_network.id
   description = "The ID of the VPC created for the ECS Todo App."
 }
+
+
+output public_subnet_id {
+  value       = aws_subnet.ecs_public_subnet.id
+  description = "The ID of the public subnet created for the ECS Todo App."
+}
