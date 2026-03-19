0e07a8ee7747e0d436cddbbc6ed18e449c12d98f
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-19 18:02:52 +0100

    exported private subnet ids

--- terraform/modules/networking/output.tf
+++ terraform/modules/networking/output.tf
@@ -8,3 +8,13 @@ output public_subnet_id {
   value       = aws_subnet.ecs_public_subnet.id
   description = "The ID of the public subnet created for the ECS Todo App."
 }
+
+output private_subnet_1_id {
+  value       = aws_subnet.ecs_private_subnet_1.id
+  description = "The ID of the private subnet 1 created for the ECS Todo App."
+}
+
+output private_subnet_2_id {
+  value       = aws_subnet.ecs_private_subnet_2.id
+  description = "The ID of the private subnet 2 created for the ECS Todo App."
+}
