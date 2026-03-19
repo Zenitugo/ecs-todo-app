0ea189332adf90dbc165b1a00418a21c16cefe98
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-19 17:44:58 +0100

    declared variables for private subnets

--- terraform/modules/networking/variable.tf
+++ terraform/modules/networking/variable.tf
@@ -2,4 +2,6 @@ variable vpc_cidr_block {}
 variable name {}
 variable public_subnet_cidr_block {}
 variable availability_zone {}
+variable private_subnet_1_cidr_block {}
+variable private_subnet_2_cidr_block {}
 
