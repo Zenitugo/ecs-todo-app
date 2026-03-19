1a3df12fe9b3027a3b41ab14435afd3885a62f4c
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-19 17:46:25 +0100

    network module updated to include private subnets

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -4,6 +4,8 @@ module "networking" {
   vpc_cidr_block                      =  var.vpc_cidr_block
   public_subnet_cidr_block            =  var.public_subnet_cidr_block
   availability_zone                   =  var.availability_zone
+  private_subnet_1_cidr_block         =  var.private_subnet_1_cidr_block
+  private_subnet_2_cidr_block         =  var.private_subnet_2_cidr_block
 }
 
 
