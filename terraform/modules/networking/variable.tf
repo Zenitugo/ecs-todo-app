8652fbbaf6b80199815d4181342a25e74456ddba
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-20 13:49:46 +0100

    second public subnet variable added

--- terraform/modules/networking/variable.tf
+++ terraform/modules/networking/variable.tf
@@ -1,7 +1,7 @@
 variable vpc_cidr_block {}
 variable name {}
 variable public_subnet_cidr_block {}
-variable availability_zone {}
 variable private_subnet_1_cidr_block {}
 variable private_subnet_2_cidr_block {}
+variable public_subnet_cidr_block_2 {}
 
