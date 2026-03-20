7851af39c4a0f72d32ad7e6f2a7421b03ee73460
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-20 15:24:34 +0100

    instance profile variable declared

--- terraform/modules/ec2/variable.tf
+++ terraform/modules/ec2/variable.tf
@@ -1,7 +1,7 @@
 variable instance_type {}
 variable key_name {}
 variable private_subnet_1_id {}
-variable sg_ids {}
 variable name {}
 variable key_filename {}
 variable ec2_sg_ids {}
+variable ec2_instance_profile_name {}
