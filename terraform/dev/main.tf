ada42a88306cd9b9d04a589dbc4d22dedac0abd3
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-20 13:36:21 +0100

    debugged made changes to file, and included iam modules

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -3,9 +3,9 @@ module "networking" {
   name                                =  var.name
   vpc_cidr_block                      =  var.vpc_cidr_block
   public_subnet_cidr_block            =  var.public_subnet_cidr_block
-  availability_zone                   =  var.availability_zone
   private_subnet_1_cidr_block         =  var.private_subnet_1_cidr_block
   private_subnet_2_cidr_block         =  var.private_subnet_2_cidr_block
+  public_subnet_cidr_block_2          =  var.public_subnet_cidr_block_2
 }
 
 
@@ -20,10 +20,12 @@ module "ec2" {
   source                             = "../modules/ec2"
   instance_type                      = var.instance_type
   key_name                           = var.key_name
-  public_subnet_id                   = module.networking.private_subnet_1_id
+  private_subnet_1_id                = module.networking.private_subnet_1_id
   ec2_sg_ids                         = module.SG.ec2_sg_ids
   name                               = var.name
   key_filename                       = var.key_filename
+  ec2_instance_profile_name          = module.iam.ec2_instance_profile_name
+
 }
 
 module "ecr" {
@@ -41,13 +43,29 @@ module "ecs" {
   private_subnet_2_id                = module.networking.private_subnet_2_id
   ecs_sg_ids                         = module.SG.ecs_sg_ids
   target_group_arn                   = module.lb.target_group_arn
+  execution_role_arn                 = module.iam.execution_role_arn
 }
 
 module "lb" {
   source                             = "../modules/lb"
   name                               = var.name
-  public_subnet_id                   = module.networking.ecs_public_subnet_id
+  public_subnet_id                   = module.networking.public_subnet_id
   load_balancer_sg_ids               = module.SG.load_balancer_sg_ids
   container_port                     = var.container_port
   vpc_id                             = module.networking.vpc_id
+  public_subnet_id_2                 = module.networking.public_subnet_2_id
+}
+
+module "iam" {
+  source                             = "../modules/iam"
+  bucket_name                        = module.s3.bucket_name
+  region                             = var.aws_region
+  ecr_repository_name                = module.ecr.ecr_repository_name
+  cluster_name                       = module.ecs.cluster_name
+  service_name                       = module.ecs.service_name
+}
+
+module "s3" {
+  source                             = "../modules/s3"
+  name                               = var.name
 }
