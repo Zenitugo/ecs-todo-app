b3c17fa9fc8ffd10d1e0d8c42f2ad0aa26871201
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-20 13:50:22 +0100

    public subnet 2 id exported

--- terraform/modules/networking/output.tf
+++ terraform/modules/networking/output.tf
@@ -18,3 +18,7 @@ output private_subnet_2_id {
   value       = aws_subnet.ecs_private_subnet_2.id
   description = "The ID of the private subnet 2 created for the ECS Todo App."
 }
+output  public_subnet_2_id {
+  value       = aws_subnet.ecs_public_subnet_2.id
+  description = "The ID of the public subnet 2 created for the ECS Todo App."
+}
