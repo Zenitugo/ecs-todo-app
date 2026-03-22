3a93b462e424d9a455ded2095998393478b63708
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-22 13:01:13 +0100

    changed region because of service quotas and bucket name

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "ugochi-terraform-state"
+    bucket = "ugochi-project1-state"
     key    = "todo-app/ecs/terraform.tfstate"
-    region = "eu-central-1"
+    region = "us-east-1"
     dynamodb_table = "ugochi-terraform-lock"
     encrypt = true
   }
