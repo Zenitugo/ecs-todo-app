ab8a7a8eae29920063fd64de3de13f7ff5a86fe8
zenitugo <ugochiukaegbu19@gmail.com>
2026-03-30 11:33:20 +0100

    changed the data ami for ec2 to the one with ecs agent

--- terraform/modules/ec2/data.tf
+++ terraform/modules/ec2/data.tf
@@ -1,11 +1,12 @@
-data "aws_ami" "ubuntu" {
+# ECS-optimized Amazon Linux 2 AMI
+data "aws_ami" "ecs_ami" {
   most_recent = true
 
-  owners = ["099720109477"] # Canonical
+  owners = ["amazon"] # Amazon
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/*/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["amzn2-ami-ecs-hvm-*-x86_64-ebs"]
   }
 
   filter {
@@ -23,4 +24,38 @@ data "aws_ami" "ubuntu" {
     values = ["x86_64"]
   }
 }
+
+
+
+
+
+
+
+
+
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+
+#   owners = ["099720109477"] # Canonical
+
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/*/ubuntu-noble-24.04-amd64-server-*"]
+#   }
+
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+
+#   filter {
+#     name   = "root-device-type"
+#     values = ["ebs"]
+#   }
+
+#   filter {
+#     name   = "architecture"
+#     values = ["x86_64"]
+#   }
+# }
 
